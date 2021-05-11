fcf361c9c9a54dfab7bcdfc4b6d709cda255c69c
Evandro Begati <begati@projetusti.com.br>
2021-05-11 11:09:36 -0300

    fix

--- mint-config.sh
+++ mint-config.sh
@@ -24,7 +24,6 @@ apt-get dist-upgrade -y
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 
 apt-get install \
- notion-desktop \
  cabextract \
  ubuntu-restricted-extras \
  remmina \
