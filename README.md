ba05e25e2b2710d9ea66542b3387410720938e9b
Evandro Begati <begati@projetusti.com.br>
2024-05-17 12:47:20 -0300

    Removido debian, add fedora.

--- README.md
+++ README.md
@@ -2,33 +2,25 @@
 
 Some scripts that helps me to get ready for work on a clean Linux installation.
 
-# Debian (12 stable)
+# Fedora 40
 
 ```bash
-su -
-usermod -aG sudo CHANGE_FOR_YOUR_USER
-apt update; apt install curl -y
-shutdown -r now
-
-```
-```bash
-curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/debian-config.sh | sudo bash
-
+curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/fedora-config.sh | sudo bash
 ```
 
-# Linux Mint - Infra (21.x)
+# Linux Mint - Infra (22.x)
 
 ```bash
 curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/mint-config-infra.sh | sudo bash
 ```
 
-# Linux Mint - QA (21.x)
+# Linux Mint - QA (22.x)
 
 ```bash
 curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/mint-config-qa.sh | sudo bash
 ```
 
-# Linux Mint - Suporte (21.x)
+# Linux Mint - Suporte (22.x)
 
 ```bash
 curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/mint-config-suporte.sh | sudo bash
@@ -38,10 +30,4 @@ curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/m
 
 ```bash
 curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/popos-config.sh | sudo bash
-```
- 
-# Fedora (> 34)
-
-```bash
-curl -s https://raw.githubusercontent.com/begati/linux-environment-helper/main/fedora-config.sh | sudo bash
 ```
--- fedora-config.sh
+++ fedora-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # Description: Script for enviroment configuration for Fedora 40
 # Author: Evandro Begati
-# Date: 2021/04/29
+# Date: 2024/05/17
 
 # Verify for sudo/root execution
 if [ "$EUID" -ne 0 ]
@@ -16,12 +16,10 @@ RUID=$(who | awk 'FNR == 1 {print $1}')
 RUSER_UID=$(id -u ${RUID})
 
 # Configurar o DNF
-#echo 'fastestmirror=1' | tee -a /etc/dnf/dnf.conf
-echo 'max_parallel_downloads=10' | tee -a /etc/dnf/dnf.conf
+echo 'fastestmirror=true' | tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=20' | tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | tee -a /etc/dnf/dnf.conf
 
-# Enable fstrim
-systemctl enable fstrim.timer
 
 # Full system upgrade
 dnf upgrade --refresh -y
@@ -33,34 +31,36 @@ dnf group update core -y
 
 # Install basic packages
 dnf install \
- remmina \
- filezilla \
  p7zip \
- java-1.8.0-openjdk-devel \
- java-11-openjdk \
+ java-11-openjdk-devel \
  docker \
  docker-compose \
+ oci-cli \
  htop \
  openssh-askpass \
  gnome-extensions-app \
  gnome-tweaks \
  gnome-shell-extension-appindicator \
+ gnome-shell-extension-caffeine \
+ gnome-shell-extension-dash-to-dock \
  fira-code-fonts \
- flameshot \
- zram \
- vlc \
+ adw-gtk3-theme \
+ zsh \
+ zsh-autosuggestions \
  -y
 
 # Install Python dev packages
 dnf install \
  python-wheel \
+ python-setuptools-wheel \
  python-devel \
  python-virtualenv \
+ python-pip \
  -y
 
 # Install Windows fonts
 dnf install curl cabextract xorg-x11-font-utils fontconfig -y
-rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
 
 # Install Windows 10 fonts
 sudo -u $SUDO_USER mkdir /home/$SUDO_USER/.fonts
@@ -83,18 +83,15 @@ dnf install code -y
 dnf install https://download.teamviewer.com/download/linux/version_13x/teamviewer.x86_64.rpm -y
 dnf config-manager --set-disabled teamviewer
 
-# Configure Docker
-usermod -aG docker $SUDO_USER
-systemctl enable docker
-systemctl start docker
+# Install OpenLens
+wget "https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.2-366/OpenLens-6.5.2-366.x86_64.rpm" -O openlens.rpm
+dnf install openlens.rpm -y
+rm -Rf openlens.rpm
 
-# Enable swap
-btrfs subvolume create /var/swap
-chattr +C /var/swap
-fallocate -l 16G /var/swap/swapfile1
-mkswap /var/swap/swapfile1
-swapon /var/swap/swapfile1
-echo "/var/swap/swapfile1 swap swap defaults 0 0" >> /etc/fstab
+# Install Bitrix24
+wget https://dl.bitrix24.com/b24/bitrix24_desktop.rpm -O bitrix.rpm
+dnf install bitrix.rpm -y
+rm -Rf bitrix.rpm
 
 # Install PyCharm
 dnf config-manager --set-enabled phracek-PyCharm
@@ -107,47 +104,48 @@ sudo -u $SUDO_USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1
 
 # Enable flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak update
 
 # Instalar pacotes via flatpak
-sudo -u $SUDO_USER flatpak install flathub io.dbeaver.DBeaverCommunity -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.anydesk.Anydesk -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.getpostman.Postman -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.spotify.Client -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.qbittorrent.qBittorrent -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.simplenote.Simplenote -y --noninteractive
+sudo -u $SUDO_USER flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+sudo -u $SUDO_USER flatpak install flathub org.flameshot.Flameshot -y --noninteractive
 sudo -u $SUDO_USER flatpak install flathub com.uploadedlobster.peek -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gimp.GIMP -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.obsproject.Studio -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.telegram.desktop -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub org.remmina.Remmina -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub org.flameshot.Flameshot -y --noninteractive
+sudo -u $SUDO_USER flatpak install io.dbeaver.DBeaverCommunity -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub hu.irl.cameractrls -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub org.gnome.Cheese -y --noninteractive
+sudo -u $SUDO_USER flatpak install com.getpostman.Postman -y --noninteractive
+sudo -u $SUDO_USER flatpak install com.spotify.Client -y --noninteractive
+sudo -u $SUDO_USER flatpak install org.gimp.GIMP -y --noninteractive
+sudo -u $SUDO_USER flatpak install com.obsproject.Studio -y --noninteractive
+sudo -u $SUDO_USER flatpak install org.telegram.desktop -y --noninteractive
+sudo -u $SUDO_USER flatpak install org.kde.kdenlive -y --noninteractive
+sudo -u $SUDO_USER flatpak install com.github.tchx84.Flatseal -y --noninteractive
+sudo -u $SUDO_USER flatpak install us.zoom.Zoom -y --noninteractive
+sudo -u $SUDO_USER flatpak install flathub org.videolan.VLC -y --noninteractive
 sudo -u $SUDO_USER flatpak install flathub com.discordapp.Discord
 sudo -u $SUDO_USER flatpak override --filesystem=home com.discordapp.Discord
-sudo -u $SUDO_USER flatpak install flathub org.kde.kdenlive -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub com.github.tchx84.Flatseal -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub us.zoom.Zoom -y --noninteractive
-sudo -u $SUDO_USER flatpak update -y --noninteractive
-
-# Set Flameshot as default print screen application
-wget --no-check-certificate https://raw.githubusercontent.com/begati/gnome-shortcut-creator/main/gnome-keytool.py -O gnome-keytool.py
-sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
-sudo -u $SUDO_USER python3 gnome-keytool.py 'Print Screen' 'flameshot gui' 'Print'
-rm -Rf gnome-keytool.py
 
 # Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -rf kubectl
 
+# Install kubectx and kubens
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 # Adicionar chave SSH ao sistema
-update-crypto-policies --set DEFAULT:FEDORA32
-sudo -u $SUDO_USER ssh-keygen -q -t rsa -N '' -f /home/$SUDO_USER/.ssh/id_rsa
+sudo -u $SUDO_USER ssh-keygen -q -t ed25519 -N '' -f /home/$SUDO_USER/.ssh/ed25519
 clear
 echo "Abra https://bitbucket.org/account/settings/ssh-keys/ no seu browser e faça a adição da chave acima."
 echo ""
-cat /home/$SUDO_USER/.ssh/id_rsa.pub
+cat /home/$SUDO_USER/.ssh/id_ed25519.pub
 echo ""
 read -p "Quando estiver pronto, pressione qualquer tecla para continuar... " temp </dev/tty
 
@@ -161,6 +159,25 @@ echo "E-mail: "
 read email </dev/tty
 sudo -u $SUDO_USER git config --global user.name "$nome"
 sudo -u $SUDO_USER git config --global user.email "$email"
+
+# Configure Docker
+usermod -aG docker $SUDO_USER
+systemctl enable docker
+systemctl start docker
+
+# Set adw-gtk3-dark theme for legacy apps
+sudo -u $SUDO_USER gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+sudo -u $SUDO_USER gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# Allow volume above 100%
+sudo -u $SUDO_USER gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true'
+
+# Set zsh as default
+sudo -u $SUDO_USER chsh -s $(which zsh)
+
+# Install OhMyZSH
+sudo -u $SUDO_USER sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 clear
 
 # Clean
