12d4c2e7749c0aa838417ee3b40a2f31c9b6c413
Evandro Begati <evandroalb@gmail.com>
2024-07-24 12:19:56 -0300

    Update mint-config-infra.sh

--- mint-config-infra.sh
+++ mint-config-infra.sh
@@ -12,15 +12,6 @@ fi
 # Auto accept EULA for MS TTF
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 
-# Add Notion Enhanced repository
-echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | tee /etc/apt/sources.list.d/notion-repackaged.list
-
-# Remove unnecessary packages
-apt-get purge -y \
-  "thunderbird*" \
-  "hexchat*" \
-  "libreoffice*"
-
 # Full system upgrade
 apt-get update
 apt-get -f install -y
@@ -28,7 +19,6 @@ apt-get dist-upgrade -y
 
 # Install base packages
 apt-get install -y \
- linux-oem-22.04b \
  cabextract \
  ubuntu-restricted-extras \
  gnome-boxes \
@@ -36,8 +26,7 @@ apt-get install -y \
  zip \
  unzip \
  p7zip-full \
- openjdk-11-jre \
- openjdk-8-jdk \
+ openjdk-17-jre \
  git \
  git-flow \
  docker.io \
@@ -47,10 +36,8 @@ apt-get install -y \
  ssh-askpass \
  zram-config \
  zsh \
- fonts-firacode \
- caffeine \
- notion-app-enhanced
-
+ fonts-firacode
+ 
 # Install Python dev packages
 apt-get install \
  python3-pip \
@@ -67,7 +54,7 @@ sudo -u $SUDO_USER wget -qO- http://plasmasturm.org/code/vistafonts-installer/vi
 rm -rf /home/$SUDO_USER/PowerPointViewer.exe
   
 # Install Facilitador Linux
-curl -s https://raw.githubusercontent.com/projetus-ti/facilitador-linux/master/install.sh | bash
+# curl -s https://raw.githubusercontent.com/projetus-ti/facilitador-linux/master/install.sh | bash
 
 # Install Discord
 wget --no-check-certificate "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
@@ -83,11 +70,6 @@ rm -Rf chrome.deb
 wget --no-check-certificate "https://download.teamviewer.com/download/linux/version_13x/teamviewer_amd64.deb" -O teamviewer.deb
 dpkg -i teamviewer.deb
 rm -Rf teamviewer.deb
-
-# Install Calima App
-wget --no-check-certificate "https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/id3qvymhlwic/b/downloads/o/calima-app/calima-app-2.0.15.deb" -O calima.deb
-dpkg -i calima.deb
-rm -Rf calima.deb
 
 # Install VSCode
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
@@ -130,16 +112,6 @@ usermod -aG docker $SUDO_USER
 # Fix for IntelliJ/PyCharm
 echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
 
-# Set Swap and Zram
-swapoff /swapfile
-rm -rf /swapfile
-dd if=/dev/zero of=/swapfile bs=8192 count=1048576
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-systemctl enable zram-config
-systemctl start zram-config
-
 # Install Windows 10 fonts
 sudo -u $SUDO_USER mkdir /home/$SUDO_USER/.fonts
 sudo -u $SUDO_USER wget -qO- http://plasmasturm.org/code/vistafonts-installer/vistafonts-installer | sudo -u $SUDO_USER bash
@@ -150,7 +122,6 @@ sudo -u $SUDO_USER flatpak install flathub com.uploadedlobster.peek -y --noninte
 sudo -u $SUDO_USER flatpak install flathub org.remmina.Remmina -y --noninteractive
 sudo -u $SUDO_USER flatpak install flathub org.flameshot.Flameshot -y --noninteractive
 sudo -u $SUDO_USER flatpak install io.dbeaver.DBeaverCommunity -y --noninteractive
-sudo -u $SUDO_USER flatpak install com.anydesk.Anydesk -y --noninteractive
 sudo -u $SUDO_USER flatpak install flathub io.github.mimbrero.WhatsAppDesktop
 sudo -u $SUDO_USER flatpak install flathub hu.irl.cameractrls -y --noninteractive
 sudo -u $SUDO_USER flatpak install com.getpostman.Postman -y --noninteractive
@@ -162,43 +133,7 @@ sudo -u $SUDO_USER flatpak install org.kde.kdenlive -y --noninteractive
 sudo -u $SUDO_USER flatpak install com.github.tchx84.Flatseal -y --noninteractive
 sudo -u $SUDO_USER flatpak install us.zoom.Zoom -y --noninteractive
 sudo -u $SUDO_USER flatpak install flathub org.videolan.VLC -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.libreoffice.LibreOffice -y --noninteractive
-sudo -u $SUDO_USER flatpak remove org.kde.Kstyle.Adwaita -y --noninteractive
 sudo -u $SUDO_USER flatpak update -y --noninteractive
-
-# Flatpak desktop integration with Linux Mint
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Teal  -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Red -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Purple -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Pink -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Orange -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Grey -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Teal -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Sand -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Red -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Purple -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Pink -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Orange -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Grey -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Brown -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Blue -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Darker-Aqua -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Teal -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Sand -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Red -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Purple -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Pink -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Orange -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Grey -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Brown -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Blue -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Dark-Aqua -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Brown -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Blue -y --noninteractive
-sudo -u $SUDO_USER flatpak install flathub org.gtk.Gtk3theme.Mint-Y-Aqua -y --noninteractive
 
 # Clean
 apt clean
